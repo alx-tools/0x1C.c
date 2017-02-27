bf48d5cb555f5868e31b4090f69a41f793aacd2c
Alexandre Gautier <alexandre.gautier@epitech.eu>
2017-02-27 10:34:41 +0300

    Keep compact mode only

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -24,7 +24,6 @@ int print_t(const binary_tree_t *tree, int is_left, int offset, int depth, char 
 	left = print_t(tree->left, 1, offset, depth + 1, s);
 	right = print_t(tree->right, 0, offset + left + width, depth + 1, s);
 
-#ifdef COMPACT
 	for (int i = 0; i < width; i++)
 		s[depth][offset + left + i] = b[i];
 
@@ -40,24 +39,6 @@ int print_t(const binary_tree_t *tree, int is_left, int offset, int depth, char 
 			s[depth - 1][offset - width / 2 + i] = '-';
 		s[depth - 1][offset + left + width / 2] = '.';
 	}
-#else
-	for (int i = 0; i < width; i++)
-		s[2 * depth][offset + left + i] = b[i];
-	if (depth && is_left)
-	{
-		for (int i = 0; i < width + right; i++)
-			s[2 * depth - 1][offset + left + width / 2 + i] = '-';
-		s[2 * depth - 1][offset + left + width / 2] = '+';
-		s[2 * depth - 1][offset + left + width + right + width / 2] = '+';
-	}
-	else if (depth && !is_left)
-	{
-		for (int i = 0; i < left + width; i++)
-			s[2 * depth - 1][offset - width / 2 + i] = '-';
-		s[2 * depth - 1][offset + left + width / 2] = '+';
-		s[2 * depth - 1][offset - width / 2 - 1] = '+';
-	}
-#endif
 	return (left + width + right);
 }
 
