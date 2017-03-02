e1fe30fbc99061324999e19e9da7ecfb771585c2
Alexandre Gautier <alexandre.gautier@epitech.eu>
2017-03-02 18:35:47 +0300

    Removed R-B tree print function

--- binary_trees.h
+++ binary_trees.h
@@ -19,37 +19,6 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-/**
- * enum rb_color_e - Possible color of a Red-Black tree
- *
- * @RED: 0 -> Red node
- * @BLACK: 1 -> Black node
- */
-typedef enum rb_color_e
-{
-	RED = 0,
-	BLACK
-} rb_color_t;
-
-/**
- * struct rb_tree_s - Red-Black tree node structure
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- * @color: Color of the node (RED or BLACK)
- */
-typedef struct rb_tree_s
-{
-	int n;
-	struct rb_tree_s *parent;
-	struct rb_tree_s *left;
-	struct rb_tree_s *right;
-	rb_color_t color;
-} rb_tree_t;
-
 void binary_tree_print(const binary_tree_t *);
-void rb_tree_print(const rb_tree_t *);
 
 #endif /* _BINARY_TREES_H_ */
